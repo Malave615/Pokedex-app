0e0b04196621b33af8f69451c99fdf5bdae2fdf2
Malave615 <malavetracy@gmail.com>
2024-01-10 15:56:59 -0500

    Added an array of Pokemon
    Added an array of 4 pokemon with an array of characteristics.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,35 @@
-alert('Hello world');
+let pokemonList = [
+  {
+	name: 'Pikachu',
+	height: '0.4m',
+	type: 'electric',
+	abilities: ['static', 'lightningrod']
+  },
+  {
+  	name: 'Jigglypuff',
+  	height: '0.5m',
+  	type: ['fairy', 'normal'],
+  	abilities: ['cute-charm', 'friend-guard']
+  },
+  {
+  	name: 'Mewtwo',
+  	height: '2m',
+  	type: 'psychic',
+  	abilities: ['pressure', 'unnerve'],
+  },
+  {
+  	name: 'Squirtle',
+  	height: '0.5m',
+  	type: 'water',
+  	abilities: ['rain-dish', 'torrent']
+  }];
 
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
 
-let name = 'John';
-document.write('My name is ' + name + ' and I am ready to start');
+
+	
+
+
+
+
+
+
