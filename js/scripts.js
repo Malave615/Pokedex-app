1afc93cb6c222e01f9d0619781daab870a9f637c
Malave615 <malavetracy@gmail.com>
2024-01-10 20:30:15 -0500

    Changed height to number format
    changed height values from sting to number format, also made type values to arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 let pokemonList = [
   {
 	name: 'Pikachu',
-	height: '0.4m',
-	type: 'electric',
+	height: 0.4m,
+	type: ['electric'],
 	abilities: ['static', 'lightningrod']
   },
   {
   	name: 'Jigglypuff',
-  	height: '0.5m',
+  	height: 0.5m,
   	type: ['fairy', 'normal'],
   	abilities: ['cute-charm', 'friend-guard']
   },
   {
   	name: 'Mewtwo',
-  	height: '2m',
-  	type: 'psychic',
+  	height: 2m,
+  	type: ['psychic'],
   	abilities: ['pressure', 'unnerve'],
   },
   {
   	name: 'Squirtle',
-  	height: '0.5m',
-  	type: 'water',
+  	height: 0.5m,
+  	type: ['water'],
   	abilities: ['rain-dish', 'torrent']
-  }];
+  }]
 
 
 
