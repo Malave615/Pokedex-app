d5d3a407af8c8e509c2fcae0cd640730d8c9ca93
Malave615 <malavetracy@gmail.com>
2024-01-12 08:30:44 -0500

    Revised Exercise 1.1
    Reverted js file to resubmit exercise 1.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,7 @@
-let pokemonList = [
-  {
-	name: 'Pikachu',
-	height: 0.4m,
-	type: ['electric'],
-	abilities: ['static', 'lightningrod']
-  },
-  {
-  	name: 'Jigglypuff',
-  	height: 0.5m,
-  	type: ['fairy', 'normal'],
-  	abilities: ['cute-charm', 'friend-guard']
-  },
-  {
-  	name: 'Mewtwo',
-  	height: 2m,
-  	type: ['psychic'],
-  	abilities: ['pressure', 'unnerve'],
-  },
-  {
-  	name: 'Squirtle',
-  	height: 0.5m,
-  	type: ['water'],
-  	abilities: ['rain-dish', 'torrent']
-  }]
+alert('Hello world');
 
+let favoriteFood = 'pizza';
+document.write(favoriteFood);
 
 
 	
