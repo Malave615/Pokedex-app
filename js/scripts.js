196407611ad8ec2c1814d1cd986042b81196f454
Malave615 <malavetracy@gmail.com>
2024-01-12 08:39:44 -0500

    Reverted changes in js file
    Reverted changes i made to js file in order to resubmit exercise 1.1, back to changes for task 1.2 in order to move on to exercise 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
-
-
-	
-
-
+let pokemonList = [
+  {
+    name: 'Pikachu',
+    height: 0.4, // changed to a number
+    type: ['electric'], // changed to an array
+    abilities: ['static', 'lightningrod']
+  },
+  {
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy', 'normal'], // kept as an array
+    abilities: ['cute-charm', 'friend-guard']
+  },
+  {
+    name: 'Mewtwo',
+    height: 2,
+    type: ['psychic'],
+    abilities: ['pressure', 'unnerve']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    type: ['water'], // fixed the typo
+    abilities: ['rain-dish', 'torrent']
+  }]
 
 
 
