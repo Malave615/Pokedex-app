549752bfc0cfea6d73eefc74db12cab6c83752de
Malave615 <malavetracy@gmail.com>
2024-01-19 12:12:56 -0500

    Conditonal
    Added a conditional that hightlights large pokemon with a string 'That's a big pokemon!'.

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,13 @@ const pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++) {
   document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+  if (pokemonList[i].height <= 1.0) {
+    document.write('<br>');
+  }
+  else {
+    document.write('That\'s a big pokemon!');
+    document.write('<br>');
+  }
 }
 
 
