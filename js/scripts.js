48b6d68c7e3cd46d653a437b1b939121c4330d7a
Malave615 <malavetracy@gmail.com>
2024-01-24 22:11:50 -0500

    Created IIFE
    Created an IIFE to house my pokemonList array in order to preserve from changes. Called IIFE to value pokemonLIst.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,93 @@
-const pokemonList = [
-  {
-    name: 'Pikachu',
-    height: 0.4, // changed to a number
-    type: ['electric'], // changed to an array
-    abilities: ['static', 'lightningrod']
-  },
-  {
-    name: 'Jigglypuff',
-    height: 0.5,
-    type: ['fairy', 'normal'], // kept as an array
-    abilities: ['cute-charm', 'friend-guard']
-  },
-  {
-    name: 'Mewtwo',
-    height: 2,
-    type: ['psychic'],
-    abilities: ['pressure', 'unnerve']
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    type: ['water'], // fixed the typo
-    abilities: ['torrent']
-  }
-];
+const pokemonRepository = (function () {
+  const pokemonList = [
+    {
+      name: 'Pikachu',
+      height: 0.4, // changed to a number
+      type: ['Electric'], // changed to an array
+      abilities: ['Static', 'Lightningrod']
+    },
+    {
+      name: 'Jigglypuff',
+      height: 0.5,
+      type: ['Fairy', 'Normal'], // kept as an array
+      abilities: ['Cute-charm', 'Friend-guard']
+    },
+    {
+      name: 'Mewtwo',
+      height: 2,
+      type: ['Psychic'],
+      abilities: ['Pressure', 'Unnerve']
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      type: ['Water'], // fixed the typo
+      abilities: ['Torrent']
+    }
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
-  if (pokemonList[i].height <= 1.0) {
-    document.write('<br>');
-  }
-  else {
-    document.write('That\'s a big pokemon!');
-    document.write('<br>');
-  }
-}
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      return pokemonList;
+    },
+  };
+})();
 
-function divide (dividend, divisor) {
-  if (divisor === 0) {
-    return 'You\re trying to divide by zero.'
-  } 
-  else {
-    result = dividend / divisor;
-    return result;
-  }
-}
+pokemonRepository.getAll().forEach(function (pokemon) {
+  console.log(pokemon.name + ' is ' + pokemon.height + ' meters tall.');
+  console.log(pokemon.type + ' is his type and ' + pokemon.abilities + ' are his abilities. ');
+});
 
-console.log(divide(4, 2));
-console.log(divide(7, 0));
-console.log(divide(1, 4));
-console.log(divide(12, -3));
+
+
+
+
+
+
+
+// for (let i = 0; i < pokemonList.length; i++) {
+//   document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+//   if (pokemonList[i].height <= 1.0) {
+//     document.write('<br>');
+//   }
+//   else {
+//     document.write('That\'s a big pokemon!');
+//     document.write('<br>');
+//   }
+// }
+
+// function divide (dividend, divisor) {
+//   if (divisor === 0) {
+//     return 'You\re trying to divide by zero.'
+//   } else {
+//     result = dividend / divisor;
+//     return result;
+//   }
+// }
+
+// console.log(divide(4, 2));
+// console.log(divide(7, 0));
+// console.log(divide(1, 4));
+// console.log(divide(12, -3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
