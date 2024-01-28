e5a8b41b55fef75e8ac1f821a7b82350a8b9b7a5
Malave615 <malavetracy@gmail.com>
2024-01-27 22:30:53 -0500

    IFFE and Return
    Wrapped pokemonList in IFFE and called it with return specifications and added conditional to inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,50 @@
 const pokemonRepository = (function () {
-  const pokemonList = [
-    {
-      name: 'Pikachu',
-      height: 0.4, // changed to a number
-      type: ['Electric'], // changed to an array
-      abilities: ['Static', 'Lightningrod']
-    },
-    {
-      name: 'Jigglypuff',
-      height: 0.5,
-      type: ['Fairy', 'Normal'], // kept as an array
-      abilities: ['Cute-charm', 'Friend-guard']
-    },
-    {
-      name: 'Mewtwo',
-      height: 2,
-      type: ['Psychic'],
-      abilities: ['Pressure', 'Unnerve']
-    },
-    {
-      name: 'Squirtle',
-      height: 0.5,
-      type: ['Water'], // fixed the typo
-      abilities: ['Torrent']
-    }
-  ];
+    const pokemonList = [
+        {
+            name: 'Pikachu',
+            height: 0.4, // changed to a number
+            type: ['Electric'], // changed to an array
+            abilities: ['Static', 'Lightningrod']
+        },
+        {
+            name: 'Jigglypuff',
+            height: 0.5,
+            type: ['Fairy', 'Normal'], // kept as an array
+            abilities: ['Cute-charm', 'Friend-guard']
+        },
+        {
+            name: 'Mewtwo',
+            height: 2,
+            type: ['Psychic'],
+            abilities: ['Pressure', 'Unnerve']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            type: ['Water'], // fixed the typo
+            abilities: ['Torrent']
+        }
+    ];
 
-  return {
-    add: function (pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function () {
-      return pokemonList;
-    },
-  };
+    return {
+        add: function (pokemon) {
+            if (typeof pokemon === 'object') {
+                pokemonList.push(pokemon);
+            } else {
+                console.log('Pokemon is not correct');
+            }
+        }
+    };
+
+
+    function getAll() {
+        return pokemonList;
+    }
 })();
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + ' meters tall.');
-  console.log(pokemon.type + ' is his type and ' + pokemon.abilities + ' are his abilities. ');
+    console.log(pokemon.name + ' is ' + pokemon.height + ' meters tall.');
+    console.log(pokemon.type + ' is his type and ' + pokemon.abilities + ' are his abilities. ');
 });
 
 
